26b4c2e30600281b7cf160f379f5381f937a18be
Bruno <rafante2@gmail.com>
2019-10-31 21:16:52 +0000

    Teste react

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import AppContent from './components/app_content';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+            <AppContent />
+      </header>
+    </div>
+  );
+}
+
+export default App;
